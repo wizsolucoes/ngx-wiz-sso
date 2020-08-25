223dafee6412c9c628f0e9453be4b034474f97c2
Touré Holder <toureh@hotmail.com>
2020-08-25 18:06:09 -0300

    chore: add testing dependencies

--- src/lib/services/sso-conector.service.spec.ts
+++ src/lib/services/sso-conector.service.spec.ts
@@ -2,9 +2,13 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { SSOConectorService } from './sso-conector.service';
 
+import { NgxWizSSOModule } from '../ngx-wiz-sso.module';
+import { ssoConfig } from '../../../testing/fake_sso_config';
+
 describe('SsoConectorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports:[NgxWizSSOModule.forRoot(ssoConfig)],
       providers: [SSOConectorService]
     });
   });
