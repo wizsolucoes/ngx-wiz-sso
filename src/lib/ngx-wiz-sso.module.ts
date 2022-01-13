759cf63edcb89a824366bb2e24390677b962021f
Touré Holder <toureh@hotmail.com>
2022-01-13 15:15:03 -0300

    fix: corrigir import de ModuleWithProviders

--- src/lib/ngx-wiz-sso.module.ts
+++ src/lib/ngx-wiz-sso.module.ts
@@ -1,6 +1,5 @@
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule, APP_INITIALIZER, ModuleWithProviders } from '@angular/core';
 import { _SSOConfig, SSOConfig } from './models/sso-config';
-import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpAuthInterceptor } from './interceptors/http-auth-interceptor';
 import { SSOConectorService, init_app } from './services/sso-conector.service';
@@ -8,17 +7,25 @@ import { AuthGuard } from './guards/auth-guard';
 
 @NgModule({})
 export class NgxWizSSOModule {
-  static forRoot(config: _SSOConfig): ModuleWithProviders {    
+  static forRoot(config: _SSOConfig): ModuleWithProviders<NgxWizSSOModule> {
     return {
       ngModule: NgxWizSSOModule,
       providers: [
         SSOConectorService,
         AuthGuard,
-        { provide: APP_INITIALIZER, useFactory: init_app, deps: [SSOConectorService], multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: HttpAuthInterceptor, multi: true },
-        { provide: SSOConfig, useValue: config }
+        {
+          provide: APP_INITIALIZER,
+          useFactory: init_app,
+          deps: [SSOConectorService],
+          multi: true,
+        },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpAuthInterceptor,
+          multi: true,
+        },
+        { provide: SSOConfig, useValue: config },
       ],
-    }
+    };
   }
-
 }
